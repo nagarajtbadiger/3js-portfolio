510037b828f568e75fcb50259673ed3f92f7ba20
Nagaraj T Badiger <nagarajtbadiger11@gmail.com>
2024-12-30 11:06:47 +0530

    copy update

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -32,7 +32,7 @@ export default function Home() {
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
         <div className="absolute flex flex-col items-center text-center top-[58%] sm:top-[65%] left-1/2 -translate-y-1/2 -translate-x-1/2">
           <h1 className="font-bold  text-6xl xs:text-7xl sm:text-8xl  lg:text-9xl text-accent">
-            Nagaraj T Badiger
+            NTB
           </h1>
           <p className="font-light text-foreground text-lg">
             Meet the wizard behind this portfolio
