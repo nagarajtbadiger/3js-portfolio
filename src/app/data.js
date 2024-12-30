3613a7a5afb8c24beee0232c3192d99b45ee3bcd
Nagaraj T Badiger <nagarajtbadiger11@gmail.com>
2024-12-30 10:59:59 +0530

    first commit

---
+++ src/app/data.js
@@ -0,0 +1,74 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [ 
+  {
+    id: 1,
+    name: "Weather App",
+    description: "Weather App created with React, rapid API and openWeather API",
+    date: "December 2024",
+    demoLink: "https://weather-app-pi-cyan-83.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "iPhone 15 Pro",
+    description: "iPhone landing page created with React, ThreeJS and GSAP",
+    date: "November 2024",
+    demoLink: "https://i-phone-sooty.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Cobra Chat",
+    description: "ChatGPT Clone",
+    date: "October 2024",
+    demoLink: "https://cobra-chat.vercel.app/",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/nagarajtbadiger",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/nagaraj-t-badiger-46155a222/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://x.com/cobraden60",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
